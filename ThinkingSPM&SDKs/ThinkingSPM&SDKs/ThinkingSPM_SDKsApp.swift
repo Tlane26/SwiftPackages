18192eb5dd359a9c578a453c3f34d7b82d23b2d9
Tlanetzi Chavez <chaveztlanetzi14@gmail.com>
2024-04-15 12:05:44 +0200

    SDWebImages
    Caso de uso

---
+++ ThinkingSPM&SDKs/ThinkingSPM&SDKs/ThinkingSPM_SDKsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkingSPM_SDKsApp.swift
+//  ThinkingSPM&SDKs
+//
+//  Created by Tlanetzi Chavez Madero on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ThinkingSPM_SDKsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SDWebImage()
+        }
+    }
+}
